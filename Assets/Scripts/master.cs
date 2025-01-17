6bf080e2e8b00373aa8e15cba8ce2ba0cc6df5ae
jrobo-gith <jacktheroberts10@gmail.com>
2025-01-17 11:57:39 +0000

    Init Commit

---
+++ Assets/Scripts/master.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class master : MonoBehaviour
+{
+    public ChessBoard chessBoard;
+    public RenderPieces renderPieces;
+
+    void Start()
+    {
+        Debug.Log("Reached Master Script!");
+        chessBoard.DrawChessBoard();
+        renderPieces.DrawPieces();
+    }
+
+    void Update()
+    {
+        
+    }
+}
